2450c76c746acd7e4768930efc626e0a0a63d8c7
Yi <yifan.9916@gmail.com>
2015-09-08 15:52:41 +0200

    catadapt

--- app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/Category/CategoryAdapter.java
+++ app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/Category/CategoryAdapter.java
@@ -19,47 +19,49 @@ public class CategoryAdapter extends ArrayAdapter<CategoryItem>  {
 
 
 
-        private ArrayList<CategoryItem> categoryList;
-        private Context context;
-        private OnButtonClicklistener onButtonClicklistener = null;
+    private ArrayList<CategoryItem> categoryList;
+    private Context context;
+    private OnButtonClicklistener onButtonClicklistener = null;
 
-        public CategoryAdapter(Context context, ArrayList < CategoryItem > categoryItems, OnButtonClicklistener onButtonClicklistener) {
-            super(context, R.layout.my_category_list_item, categoryItems);
+    public CategoryAdapter(Context context, ArrayList < CategoryItem > categoryItems, OnButtonClicklistener onButtonClicklistener) {
+        super(context, R.layout.my_category_list_item, categoryItems);
 
-            this.context = context;
-            this.categoryList = categoryItems;
-            this.onButtonClicklistener = onButtonClicklistener;
+        this.context = context;
+        this.categoryList = categoryItems;
+        this.onButtonClicklistener = onButtonClicklistener;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if(v == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.my_category_list_item,null);
         }
 
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
+        CategoryItem categoryItem = categoryList.get(position);
+        if(categoryItem != null) {
+            Button nameOfGenus = (Button) v.findViewById(R.id.button_category_list_item);
+            nameOfGenus.setText(categoryItem.getName());
+            nameOfGenus.setTag(position);
+            nameOfGenus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("hallo", "" + position);
+                    if (onButtonClicklistener != null)
+                        onButtonClicklistener.onButtonClick(position);
 
-            View v = convertView;
 
-            if(v == null) {
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = inflater.inflate(R.layout.my_category_list_item,null);
-            }
+                }
 
-            CategoryItem categoryItem = categoryList.get(position);
-            if(categoryItem != null) {
-                Button nameOfGenus = (Button) v.findViewById(R.id.button_category_list_item);
-                nameOfGenus.setText(categoryItem.getName());
-                nameOfGenus.setTag(position);
-                nameOfGenus.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Log.d("hallo", "" + position);
-                        if (onButtonClicklistener != null)
-                            onButtonClicklistener.onButtonClick(position);
+            });
 
-                    }
-                });
-
-            }
-
-            return v;
         }
+
+        return v;
+    }
 
 
 }
