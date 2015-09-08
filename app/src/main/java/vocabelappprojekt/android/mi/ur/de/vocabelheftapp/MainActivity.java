0c87d4655a1e85540bd55aa31af8127de2a48e67
Yi <yifan.9916@gmail.com>
2015-09-08 16:09:10 +0200

    mainact

--- app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/MainActivity.java
+++ app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/MainActivity.java
@@ -31,8 +31,8 @@ public class MainActivity extends AppCompatActivity{
 
 
         initUI();
-        
-        
+
+
     }
 
     private void initUI() {
@@ -68,11 +68,11 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-       // MenuItem searchItem = menu.findItem(R.id.action_search);
-       // SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-       // return super.onCreateOptionsMenu(menu);
+        // MenuItem searchItem = menu.findItem(R.id.action_search);
+        // SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        // return super.onCreateOptionsMenu(menu);
         return true;
     }
 
