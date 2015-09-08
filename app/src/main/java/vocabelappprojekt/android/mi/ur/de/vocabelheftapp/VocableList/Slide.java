7b8485684bd39a5b19d11df09f2b76d6be72c994
Yi <yifan.9916@gmail.com>
2015-09-08 16:04:28 +0200

    slide

--- app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/VocableList/Slide.java
+++ app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/VocableList/Slide.java
@@ -1,8 +1,34 @@
 package vocabelappprojekt.android.mi.ur.de.vocabelheftapp.VocableList;
 
-/**
- * Created by Yi on 08/09/15.
- */
-public class Slide
-{
+import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import vocabelappprojekt.android.mi.ur.de.vocabelheftapp.R;
+
+public class Slide {
+    public static void slide_down(Context context, View v){
+
+        Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_down);
+        if(animation != null){
+            animation.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(animation);
+            }
+        }
+    }
+
+    public static void slide_up(Context context, View v){
+        Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_up);
+        if(animation != null){
+            animation.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(animation);
+            }
+        }
+
+    }
 }
