38f01df93bb553e87eaaf6cafdb45f056998fa16
Yi <yifan.9916@gmail.com>
2015-09-08 15:57:47 +0200

    detcatact

--- app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/Category/DetailCategoriesActivity.java
+++ app/src/main/java/vocabelappprojekt/android/mi/ur/de/vocabelheftapp/Category/DetailCategoriesActivity.java
@@ -8,28 +8,68 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import vocabelappprojekt.android.mi.ur.de.vocabelheftapp.EditVocableActivity;
 import vocabelappprojekt.android.mi.ur.de.vocabelheftapp.R;
+import vocabelappprojekt.android.mi.ur.de.vocabelheftapp.VocableList.VocAdapter;
+import vocabelappprojekt.android.mi.ur.de.vocabelheftapp.VocableList.VocDatabase;
+import vocabelappprojekt.android.mi.ur.de.vocabelheftapp.VocableList.VocItem;
 
-/**
- * Created by S�meyye on 18.08.2015.
- */
 public class DetailCategoriesActivity extends AppCompatActivity {
+
+    private ArrayList<VocItem> vocNames;
+    private DetailCategoriesAdapter detailCategoriesAdapter;
+    private VocDatabase voc_database;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailed_category_list);
 
-       ActionBar actionBar = getSupportActionBar();
-       actionBar.setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-
-
+        initDB();
+        initVocableList();
+        initUI();
+        updateList();
 
     }
 
+    private void initDB() {
+        voc_database = new VocDatabase(getApplicationContext());
+        voc_database.open();
+    }
 
+    private void initVocableList() {
+        vocNames = new ArrayList<VocItem>();
+    }
+
+    private void initUI() {
+        initListView();
+        initListAdapter();
+    }
+
+
+    private void initListAdapter() {
+        ListView list = (ListView) findViewById(R.id.list_of_detailed_category_list);
+        detailCategoriesAdapter = new DetailCategoriesAdapter(getApplicationContext(), vocNames);
+        list.setAdapter(detailCategoriesAdapter);
+    }
+
+    private void initListView() {
+        final ListView list = (ListView) findViewById(R.id.list_of_detailed_category_list);
+    }
+
+    private void updateList() {
+        vocNames.clear();
+        vocNames.addAll(voc_database.getAllVocItems());
+        detailCategoriesAdapter.notifyDataSetChanged();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,7 +101,6 @@ public class DetailCategoriesActivity extends AppCompatActivity {
 
     private void searchForVocable() {
     }
-
 
     private void addVocable() {
         Intent addNewVoc = new Intent (getApplicationContext(), EditVocableActivity.class);
